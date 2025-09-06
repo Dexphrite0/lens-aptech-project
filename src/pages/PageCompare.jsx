10b9892fd42b580f4261e99a043d9c86dca3da14
DESKTOP-0S3ETG7\USER <okered764@gmail.com>
2025-09-06 03:34:34 +0100

    Fix case mismatch in Cart import

--- src/pages/PageCompare.jsx
+++ src/pages/PageCompare.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useMemo, useState } from "react";
-import Cart from "../cart/cart.js";
+import Cart from "../Cart/cart.js";
 
 function useProducts() {
   const [data, setData] = useState([]);
